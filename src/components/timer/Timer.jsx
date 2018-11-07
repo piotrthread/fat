b0655f7d2b977d531814b9cc94e8cbf3550729ff
Piotr Niciejewski <piotrthread@gmail.com>
2018-11-07 23:16:52 +0100

    added start validation

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -13,69 +13,77 @@ class Timer extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            roundNumber: 8,
-            activeTime:20,
-            restTime:10,
-            roundCount: null,
-            activeCount: null,
-            restCount: null,
-            train: false
+            roundNumber: 6,
+            activeTime:0,
+            restTime:0,
+            roundCount: 0,
+            activeCount: 0,
+            restCount: 0,
+            train: true
         };
     }
+
     startTimer = (e) => {
         e.preventDefault();
-        this.setState({roundCount: this.state.roundNumber});
-        this.timer = startInterval(() => {
-            clearInterval(this.workout);
-            clearInterval(this.rest);
-            this.setState({
-                train: true
-            });
-            if(this.state.roundCount == 0){
-                this.setState({
-                    activeCount: null,
-                    restCount: null,
-                    roundCount: null
-                });
-                clearInterval(this.timer);
+        if(this.state.activeTime > 0 && this.state.restTime > 0){
+            this.setState({roundCount: this.state.roundNumber});
+            this.timer = startInterval(() => {
                 clearInterval(this.workout);
                 clearInterval(this.rest);
-            }else{
                 this.setState({
-                    activeCount: this.state.activeTime,
-                    restCount: this.state.restTime
+                    train: true
                 });
-                setTimeout(() => {
-                    this.rest = startInterval(() => {
-                        this.setState({
-                            restCount: this.state.restCount -1
-                        });
+                if(this.state.roundCount == 0){
+                    this.setState({
+                        activeCount: null,
+                        restCount: null,
+                        roundCount: null
+                    });
+                    clearInterval(this.timer);
+                    clearInterval(this.workout);
+                    clearInterval(this.rest);
+                }else{
+                    this.setState({
+                        activeCount: this.state.activeTime,
+                        restCount: this.state.restTime
+                    });
+                    setTimeout(() => {
+                        this.rest = startInterval(() => {
+                            this.setState({
+                                restCount: this.state.restCount -1
+                            });
+                        },1000);
+                    },(this.state.activeTime + 2) * 1000);
+                    this.workout = setInterval(() => {
+                        if(this.state.activeCount == 0){
+                            this.setState({
+                                roundCount: this.state.roundCount - 1,
+                                train: false
+                                }, () => {
+                                clearInterval(this.workout);
+                            });
+                        }else{
+                            this.setState({
+                                activeCount: this.state.activeCount - 1
+                            });
+                    }   
                     },1000);
-                },(this.state.activeTime + 2) * 1000);
-                this.workout = setInterval(() => {
-                    if(this.state.activeCount == 0){
-                        this.setState({
-                            roundCount: this.state.roundCount - 1,
-                            train: false
-                            }, () => {
-                            clearInterval(this.workout);
-                        });
-                    }else{
-                        this.setState({
-                            activeCount: this.state.activeCount - 1
-                        });
-                }   
-                },1000);
-            }
-        },(this.state.activeTime + this.state.restTime + 2) * 1000);
+                }
+            },(this.state.activeTime + this.state.restTime + 2) * 1000);
+        }
     }
+
     render(){
         return <React.Fragment>
-                {this.state.train
-                ?<Active currActive={this.state.activeCount} allActive={this.state.activeTime}/>
-                :<Rest currRest={this.state.restCount} allRest={this.state.restTime}/>}
-                <Round currRound={this.state.roundCount} allRound={this.state.roundNumber}/><br/>
-                <button className="startButton" onClick={this.startTimer}>START</button>
+            <div className="fat">
+                <div className="intervalCounter">
+                    {this.state.train
+                        ?<Active currActive={this.state.activeCount} allActive={this.state.activeTime}/>
+                        :<Rest currRest={this.state.restCount} allRest={this.state.restTime}/>}
+                        <Round currRound={this.state.roundCount} allRound={this.state.roundNumber}/>
+                        <button className="startButton" onClick={this.startTimer}>START</button>
+                </div>
+            </div>
         </React.Fragment>;
     }
 }
